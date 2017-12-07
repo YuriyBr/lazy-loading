dfa36b425fcb91fdbe8cc115d5a7b754cac169b3
ilajosmanov <illiaosmanov@gmail.com>
2017-12-07 03:44:06 +0200

    lazy-loading v2.0. added pagination and upd prev and next functions

--- js/main.js
+++ js/main.js
@@ -8,13 +8,20 @@ function init () {
         this.currentCount = 1;
         this.root = 'https://jsonplaceholder.typicode.com/photos';
         this.translateX = 0; 
+        this.widthPerOneImage = 600;
         
         // DOM
         this.listElement = document.querySelector('.slider__list');
+        this.dotsList = document.querySelector('.dots__list');
         this.preloader = document.querySelector('.preloader');
-        this.next = document.querySelector('#next');
-        this.prev = document.querySelector('#prev');
+        this.btns = document.querySelectorAll('.btn');
        
+
+        /**
+         * 
+         *  todo: Get API images
+         * 
+         */
         this._getData = (url, slide) => {
             fetch(`${url}/${slide}`).then(response => {
                 return response.json();
@@ -24,22 +31,36 @@ function init () {
             
         }
 
+        /**
+         * 
+         * todo: Append image in LI element if we not have
+         * 
+         */
+
         this.appendImage = obj => {
             let activeSlide = document.querySelector('.active-slide');
-            const img = new Image();
+            let img = new Image();
             img.src = obj.url;
             img.addEventListener('load', () => {
                 this.preloader.classList.add('preloader--hide');
-            })
+            });
             activeSlide.appendChild(img);
-        }
+        };
+
+        /**
+         * 
+         * todo: Change active slide
+         * 
+         */
 
         this.activeSlide = current => {
-            
             for (let i = 0; i < this.listElement.children.length; i++) {
                 this.listElement.children[i].classList.remove('active-slide');
                 if (i + 1 === current) {
                     this.listElement.children[i].classList.add('active-slide');
+
+                    let dataAttr = this.listElement.children[i].dataset.slide;
+                    this.activeDots(dataAttr);
 
                     if (this.listElement.children[i].children.length === 0) {
                         this.preloader.classList.remove('preloader--hide');
@@ -47,50 +68,92 @@ function init () {
                     }
                 }
             }
-        }
-
+        };
 
         /**
          * 
-         *  CONTROLS
+         * todo: Prev & Next actions
          * 
          */
 
-        this._prevElement = () => {
-            if (this.currentCount > 0) {
-                this.currentCount--;
-                this.translateX -= 600;
-                this.listElement.style.transform = `translateX(-${this.translateX}px)`;
-                this.activeSlide(this.currentCount);
+        this._prevNext = ev => {
+            let id = ev.currentTarget.getAttribute('id');
+            if (id === 'next') {
+                if (this.currentCount < this.maxCount) {
+                    this.currentCount++;
+                    this.translateX += this.widthPerOneImage;
+                }
+            } else {
+                if (this.currentCount > 0) {
+                    this.currentCount--;
+                    this.translateX -= this.widthPerOneImage;
+                }
             }
+            this.listElement.style.transform = `translateX(-${this.translateX}px)`;
+            this.activeSlide(this.currentCount);
         }
 
-        this._nextElement = () => {
-            if (this.currentCount < this.maxCount) {
-                this.currentCount++;
-                this.translateX += 600;
-                this.listElement.style.transform = `translateX(-${this.translateX}px)`;
-                
-                this.activeSlide(this.currentCount);
-            }
-        } 
+        /**
+         * 
+         *  todo: Dots actions & dots active class
+         * 
+         */
+        
+        this.activeDots = data => {
+            let elements = document.querySelectorAll('.dots__item');
+            elements.forEach(el => {
+                el.classList.remove('dots__item--active');
+                return;
+            });
+            elements.forEach(el => {
+                if (el.dataset.slide === data) {
+                    el.classList.add('dots__item--active');
+                    return;
+                }
+            });
+        };
 
-        this._events = () => {
-            this.prev.addEventListener('click', this._prevElement);
-            this.next.addEventListener('click', this._nextElement);
+        this._dotsSwitch = ev => {
+            let target = ev.target,
+                dataAttr = target.dataset.slide,
+                sliderItems = document.querySelectorAll('.slider__item');
+            this.currentCount = +dataAttr + 1;
+            this.translateX = this.widthPerOneImage * (this.currentCount - 1);
+
+            this.activeSlide(this.currentCount);
+            this.listElement.style.transform = `translateX(-${this.translateX}px)`;
+            this.activeDots(dataAttr);
         }
 
-        this._init = () => {
+        this._initLayout = (parent, childClass, activeClass) => {
             for (let i = 0; i < this.maxCount; i++) {
                 let liElement = document.createElement('li');
-                liElement.className = 'slider__item';
-                this.listElement.appendChild(liElement);
+                liElement.className = childClass;
+                liElement.setAttribute('data-slide', i);
+                parent.appendChild(liElement);
             }
-            this.listElement.children[0].classList.add('active-slide');
+            parent.children[0].classList.add(activeClass);
+        };
 
-            this._events();
+        /**
+         * 
+         * Basic constructor function's
+         * 
+         */
+        
+        this._events = () => {
+            this.btns.forEach (btn => {
+                btn.addEventListener('click', this._prevNext);
+            })
+            this.dotsList.addEventListener('click', this._dotsSwitch);
+        };
+
+        this._init = () => {
+            this._initLayout(this.listElement, 'slider__item', 'active-slide');
+            this._initLayout(this.dotsList, 'dots__item', 'dots__item--active');
             this._getData(this.root, this.currentCount);
-        }
+            this._events();
+        };
         this._init();
     }
 
